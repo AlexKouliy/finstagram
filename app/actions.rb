0c31ec21a8e909e3249ee7a109871345a42e83e3
AlexKouliy <kouliyxdk@gmail.com>
2021-11-30 00:48:36 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
@@ -6,6 +12,15 @@ end
 get '/signup' do
     @user = User.new
     erb(:signup)
+end
+
+get '/login' do
+    erb(:login)
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
 end
 
 post '/signup' do
@@ -17,8 +32,23 @@ post '/signup' do
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
     if @user.save
-        "User #{username} saved!"
+        redirect to('/login')
     else
         erb(:signup)
+    end
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+
+    user = User.find_by(username: username)
+
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)
     end
 end
